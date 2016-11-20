d18decbcef114579c8421a40a1c6ac6925e20d38
joneder1 <eder.jon@gmail.com>
2016-11-20 00:47:08 +0000

    extending the blog

--- blog/views.py
+++ blog/views.py
@@ -32,36 +32,6 @@ def entries(page=1):
         total_pages=total_pages
     )
     
-PAGINATE_BY = request.args.get('page', n)
-    
-@app.route("/?limit=n")
-@app.route("/page/?limit=n")
-def entries(page=n):
-    # Zero-indexed page
-    page_index = page - 1
-    n = request.args.get('page', 1)
-
-    count = session.query(Entry).count()
-
-    start = page_index * PAGINATE_BY
-    end = start + PAGINATE_BY
-
-    total_pages = (count - 1) // PAGINATE_BY + 1
-    has_next = page_index < total_pages - 1
-    has_prev = page_index > 0
-
-    entries = session.query(Entry)
-    entries = entries.order_by(Entry.datetime.desc())
-    entries = entries[start:end]
-
-    return render_template("entries.html",
-        entries=entries,
-        has_next=has_next,
-        has_prev=has_prev,
-        page=page,
-        total_pages=total_pages
-    )
-    
 @app.route("/entry/add", methods=["GET"])
 def add_entry_get():
     return render_template("add_entry.html")    
