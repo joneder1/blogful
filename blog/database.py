6135d94fdbae955c33e931bced362cbc512760f7
joneder1 <eder.jon@gmail.com>
2016-11-21 18:31:43 +0000

    flask-login

--- blog/database.py
+++ blog/database.py
@@ -20,5 +20,16 @@ class Entry(Base):
     title = Column(String(1024))
     content = Column(Text)
     datetime = Column(DateTime, default=datetime.datetime.now)
+    
+from flask.ext.login import UserMixin
+
+class User(Base, UserMixin):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    email = Column(String(128), unique=True)
+    password = Column(String(128))
 
 Base.metadata.create_all(engine)
+
--- blog/views.py
+++ blog/views.py
@@ -3,16 +3,26 @@ from flask import render_template
 from . import app
 from .database import session, Entry
 
+from flask import flash
+from flask import request, redirect, url_for
+from flask.ext.login import login_user
+from flask.ext.login import login_required
+from werkzeug.security import check_password_hash
+from .database import User
+
 PAGINATE_BY = 10
 
 @app.route("/")
-@app.route("/page/<int:page>")
+@app.route("/page/<int:page>/?limit=20")
+
 def entries(page=1):
     # Zero-indexed page
     page_index = page - 1
 
     count = session.query(Entry).count()
-
+    
+    PAGINATE_BY = int(request.args.get('limit', 10))
+    
     start = page_index * PAGINATE_BY
     end = start + PAGINATE_BY
 
@@ -33,12 +43,12 @@ def entries(page=1):
     )
     
 @app.route("/entry/add", methods=["GET"])
+@login_required
 def add_entry_get():
     return render_template("add_entry.html")    
-    
-from flask import request, redirect, url_for
 
 @app.route("/entry/add", methods=["POST"])
+@login_required
 def add_entry_post():
     entry = Entry(
         title=request.form["title"],
@@ -87,3 +97,19 @@ def delete_entry_post(id):
     session.delete(entry)
     session.commit()
     return redirect(url_for("entries"))
+    
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("entries"))
